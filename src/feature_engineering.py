70e7e76730e0846e9a92a030440e253fe7d34fc2
Luqman <lluqmannurhakim@gmail.com>
2025-05-08 16:51:50 +0800

    edited feature eng. script

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.exceptions import NotFittedError
 from typing import List, Tuple, Optional, Dict, Union
 
 import logging
